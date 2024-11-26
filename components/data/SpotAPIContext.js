54efc1fc0d520b5b15027621c18d2f8c3f416506
Joe Baker <joeblakeb@gmail.com>
2024-11-26 17:14:10 +0000

    Add saved spots to spots page & add getting individual spots from the api

--- components/data/SpotAPIContext.js
+++ components/data/SpotAPIContext.js
@@ -7,57 +7,85 @@ import SettingsContext from "./SettingsContext";
 
 const GET_EXTRA_DELTA = 0.4;
 
-const DEMO_SPOTS = [
-    {
-        id: "61395abb84ce2eb3d50066c8",
-        name: "Swanage",
-        lat: 50.613,
-        lon: -1.956,
-    },
-    {
-        id: "584204214e65fad6a7709cf4",
-        name: "Bournemouth",
-        lat: 50.713,
-        lon: -1.877,
-        rating: "POOR"
-    },
-    {
-        id: "584204214e65fad6a7709cee",
-        name: "Boscombe",
-        lat: 50.717,
-        lon: -1.835,
-        rating: "FLAT"
-    },
-    {
-        id: "613ba68936d5112d6d6b38b3",
-        name: "Milford on Sea",
-        lat: 50.720,
-        lon: -1.592,
-        rating: "FAIR"
-    },
-    {
-        id: "613a73bdd66c4039f3633bcc",
-        name: "Mudeford Harbour",
-        lat: 50.726,
-        lon: -1.743,
-        rating: "FAIR_TO_GOOD"
-    }
-];
-
 class Spot {
-    constructor(spot) {
+    constructor(data) {
+        if (typeof data === "string") {
+            this.id = data;
+            return;
+        }
+
+        let spot;
+        if (data._id === undefined && data.spot !== undefined) {
+            spot = data.spot;
+        } else {
+            spot = data;
+        }
+
         this.id = spot._id;
+        this.update(data);
+    }
+
+    update(data) {
+        let spot, conditions;
+        if (data._id === undefined && data.spot !== undefined) {
+            spot = data.spot;
+            conditions = data.forecast.conditions;
+        } else {
+            spot = data;
+            conditions = data.conditions;
+        }
+
         this.name = spot.name;
         this.lat = spot.lat;
         this.lon = spot.lon;
         this.photo = spot.cameras.length > 0 ? spot.cameras[0].stillUrlFull : null;
-        this.rating = spot.conditions.value;
-    }
-
-    update(spot) {
-        this.rating = spot.conditions.value;
+        this.rating = conditions.value;
     }
 };
+
+const DEMO_SPOTS = (() => {
+    let spots = {};
+    const possibleRatings = ["FLAT", "POOR", "POOR_TO_FAIR", "FAIR", "GOOD", "EPIC"];
+
+    for (let spot of [
+        {
+            _id: "61395abb84ce2eb3d50066c8",
+            name: "Swanage",
+            lat: 50.613,
+            lon: -1.956,
+        },
+        {
+            _id: "584204214e65fad6a7709cf4",
+            name: "Bournemouth",
+            lat: 50.713,
+            lon: -1.877,
+        },
+        {
+            _id: "584204214e65fad6a7709cee",
+            name: "Boscombe",
+            lat: 50.717,
+            lon: -1.835,
+        },
+        {
+            _id: "613ba68936d5112d6d6b38b3",
+            name: "Milford on Sea",
+            lat: 50.720,
+            lon: -1.592,
+        },
+        {
+            _id: "613a73bdd66c4039f3633bcc",
+            name: "Mudeford Harbour",
+            lat: 50.726,
+            lon: -1.743,
+        }
+    ]) {
+        spot.cameras = [];
+        spot.conditions = {value: possibleRatings[Math.floor(Math.random() * possibleRatings.length)]};
+        spots[spot._id] = new Spot(spot);
+    }
+
+    return spots;
+})();
 
 export const SpotAPIContext = createContext();
 
@@ -65,59 +93,60 @@ export const SpotAPIProvider = ({ children }) => {
     const { settings } = useContext(SettingsContext);
     const [storedRegions, setStoredRegions] = useState([]);
     const [fetchRegionQueue, setFetchRegionQueue] = useState([]);
+    const [storedReports, setStoredReports] = useState([]);
+    const [fetchReportQueue, setFetchReportQueue] = useState([]);
 
     /**
      * @property {Object} spots an object with Spot objects with their id as the key
      */
     const [spots, setSpots] = useState({});
 
-
     /**
      * Get the next region in the queue to of regions to fetch.
      */
-    const fetchSpots = async () => {
+    const fetchRegionSpots = async () => {
         if (fetchRegionQueue.length === 0) return;
 
-        if (settings.useRealAPI) {
-            const { top, bottom, left, right } = fetchRegionQueue[fetchRegionQueue.length - 1];
-            const response = await fetch(
-                `https://services.surfline.com/kbyg/mapview?north=${top}&south=${bottom}&west=${left}&east=${right}`,
-            );
-            if (response.status !== 200) {
-                console.error("Error fetching spots:", response.status);
-                return;
-            }
-
-            const data = await response.json();
-            
-            setSpots(prevSpots => {
-                const newSpots = { ...prevSpots };
-                data.data.spots.forEach(spotData => {
-                    if (newSpots[spotData._id]) {
-                        newSpots[spotData._id].update(spotData);
-                    } else {
-                        newSpots[spotData._id] = new Spot(spotData);
-                    }
-                });
-                return newSpots;
-            });
-        } else {
-            setSpots(DEMO_SPOTS);
-            setStoredRegions([]);
+        const { top, bottom, left, right } = fetchRegionQueue[fetchRegionQueue.length - 1];
+        const url = `https://services.surfline.com/kbyg/mapview?north=${top}&south=${bottom}&west=${left}&east=${right}`
+        const response = await fetch(url);
+        if (response.status !== 200) {
+            console.error("Error fetching spots for region:", response.status, url);
+            return;
         }
 
+        const data = await response.json();
+        
+        setSpots(prevSpots => {
+            const newSpots = { ...prevSpots };
+            data.data.spots.forEach(spotData => {
+                if (newSpots[spotData._id]) {
+                    newSpots[spotData._id].update(spotData);
+                } else {
+                    newSpots[spotData._id] = new Spot(spotData);
+                }
+            });
+            return newSpots;
+        });
+
+        setStoredRegions([...storedRegions, fetchRegionQueue[fetchRegionQueue.length - 1]]);
         setFetchRegionQueue(fetchRegionQueue.slice(0, -1));
     };
 
     useEffect(() => {
-        fetchSpots();
-    }, [fetchRegionQueue]);
+        if (settings.useRealAPI) {
+            fetchRegionSpots();
+        } else {
+            setSpots(DEMO_SPOTS);
+            setStoredRegions([]);
+        }
+    }, [fetchRegionQueue, settings.useRealAPI]);
 
     /**
      * Gets the spots for a region, and adds it to the spot list, but only if the region hasn't already been requested recently (eg, zooming in).
      * Also gets a bit extra either side, for when the user is zoomed in and is moving around slowly.
      * 
-     * @param {object} region - containing the lat/long and delta of the region to request from the API
+     * @param {object} region containing the lat/long and delta of the region to request from the API
      */
     const getSpotsForRegion = (region) => {
         const { latitude, longitude, latitudeDelta, longitudeDelta } = region;
@@ -137,7 +166,7 @@ export const SpotAPIProvider = ({ children }) => {
         };
 
         let shouldFetchThisRegion = true;
-        for (const storedRegion of storedRegions) {
+        for (const storedRegion of [...storedRegions, ...fetchRegionQueue]) {
             if (storedRegion.top === needed.top && storedRegion.bottom === needed.bottom && storedRegion.left === needed.left && storedRegion.right === needed.right) {
                 shouldFetchThisRegion = false;
                 break;
@@ -146,13 +175,78 @@ export const SpotAPIProvider = ({ children }) => {
 
         if (shouldFetchThisRegion) {
             setFetchRegionQueue([...fetchRegionQueue, toRequest]);
-            setStoredRegions([...storedRegions, toRequest]);
         }
     };
 
+    /**
+     * Get the next spot report for the next spot in the queue.
+     */
+    const fetchReportForSpot = async () => {
+        if (fetchReportQueue.length === 0) return;
+
+        const spotID = fetchReportQueue[fetchReportQueue.length - 1];
+        setFetchReportQueue(fetchReportQueue.slice(0, -1));
+        const url = `https://services.surfline.com/kbyg/spots/reports?spotId=${spotID}`;
+        const response = await fetch(url);
+        if (response.status !== 200) {
+            console.error("Error fetching spot report:", response.status, url);
+            return;
+        }
+
+        setStoredReports([...storedReports, spotID]);
+        const data = await response.json();
+
+        setSpots(prevSpots => {
+            const newSpots = { ...prevSpots };
+            if (newSpots[spotID]) {
+                newSpots[spotID].update(data);
+            } else {
+                newSpots[spotID] = new Spot(data);
+            }
+            return newSpots;
+        });
+    };
+
+    useEffect(() => {
+        if (settings.useRealAPI) {
+            fetchReportForSpot();
+        } else {
+            setStoredReports([]);
+        }
+    }, [fetchReportQueue, settings.useRealAPI]);
+
+    /**
+     * Add the spot to the report request queue, if it is not already and hasn't been got yet.
+     * 
+     * @param {string} spotIDs corresponding to the Surfline spot
+     */
+    const getReportsForSpots = (spotIDs) => {
+        setFetchReportQueue(fetchReportQueue => {
+            const newQueue = [...fetchReportQueue];
+            spotIDs.forEach(spotID => {
+                if (!newQueue.includes(spotID) && !storedReports.includes(spotID)) {
+                    newQueue.push(spotID);
+                }
+            });
+            return newQueue;
+        });
+    };
+
+    /**
+     * Get a spot, or an empty id only object if it doesn't exist yet.
+     * Does not request the spot, that must be done separately.
+     * 
+     * @param {string} spotID for the surfline spot 
+     * @returns Spot with the data for that spot
+     */
+    const getSpot = (spotID) => {
+        return spots[spotID] || new Spot(spotID);
+    };
+
     const spotAPI = {
-        spots,
-        getSpotsForRegion
+        spots, getSpot,
+        getSpotsForRegion,
+        getReportsForSpots,
     };
 
     return (
--- screens/SpotsScreen.js
+++ screens/SpotsScreen.js
@@ -2,12 +2,28 @@
  * @fileoverview This is the favorite locations tab screen, the home page, which shows the users saved locations
  */
 
+import { useContext, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import SettingsContext from "../components/data/SettingsContext";
+import SpotAPIContext from "../components/data/SpotAPIContext";
+import BeachCard from "../components/BeachCard";
 
 function SpotsScreen() {
+    const { settings } = useContext(SettingsContext);
+    const { spotAPI } = useContext(SpotAPIContext)
+
+    useEffect(() => {
+        spotAPI.getReportsForSpots(settings.favoriteSpots);
+    }, [settings.favoriteSpots]);
+
     return (
         <View style={styles.container}>
-            <Text>Spots</Text>
+            {settings.favoriteSpots.map((spotID) => (
+                <BeachCard
+                    key={spotID}
+                    spot={spotAPI.getSpot(spotID)}
+                />
+            ))}
         </View>
     );
 }
